87a296c4e5051c5ba4e596c7133a68e7b0490385
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 03:20:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 
 def assign_rooms(array)
-  array.collect do | name |
-    "Hello, #{name}! You'll be assigned to room #{n}!"
+  array.collect do | name, index |
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
