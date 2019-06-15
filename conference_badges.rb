91d55e5865c9a2b7753fee146954e94fb82eb699
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 03:13:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,10 @@ def batch_badge_creator(array)
      "Hello, my name is #{name}."
   end
 end
+
+
+def assign_rooms(array)
+  array.collect do | name |
+    n =  0
+    "Hello, #{name}! You'll be assigned to room #{n+1}!"
+end
