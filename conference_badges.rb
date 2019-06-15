d992966b100648037aa5c6e24355c72f3e246acf
Ursula Miller <ursula.miller@gmail.com>
2019-06-15 03:08:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.each do | name |
-      return "Hello, my name is #{name}."
+  array.collect do | name |
+     "Hello, my name is #{name}."
   end
 end
